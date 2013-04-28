3d2a927c62b87539d9a799078ff579b2f210afe3
Richard Lasjunies <rlasjunies@gmail.com>
2013-04-28 18:03:37 +0200

    init
    client / server folders

---
+++ server/Database/db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `paints` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `year` varchar(45) DEFAULT NULL,
+  `picture` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=94 ;
