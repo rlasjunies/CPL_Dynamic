f2771d9e99f8097aaa078b26d8772700055e3a03
Richard Lasjunies <rlasjunies@gmail.com>
2013-04-28 19:20:03 +0200

    rest redesign
    * create paints.php
    * create pictures.php ( include in uploader )
    * refactor rest.php to only have routes

--- server/rest.php
+++ server/rest.php
@@ -2,12 +2,16 @@
  
 require 'Slim/Slim.php';
 include 'dbconnection.php';
+include 'paints.php';
+include 'pictures.php';
+
 Slim\Slim::registerAutoloader();
 
 use Slim\Slim;
  
 $app = new \Slim\Slim();
  
+/*  PAINTS  */
 $app->get('/paints', 'getPaints');
 $app->options('/paints', 'getPaints');
 $app->get('/paints/:id',  'getPaint');
@@ -16,120 +20,11 @@ $app->post('/paints', 'addPaint');
 $app->put('/paints/:id', 'updatePaint');
 $app->delete('/paints/:id',   'deletePaint');
 
- 
+/* PICTURES */
+$app->get('/pictures', 'getPictures');
+$app->post('/pictures', 'addPicture');
+$app->delete('/pictures/:id', 'deletePicture');
+  
 $app->run();
- 
-function getPaints() {
-    $sql = "select * FROM paints ORDER BY name";
-    try {
-        $db = getConnection();
-        $stmt = $db->query($sql);
-        $paints = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        //echo '{"paint": ' . json_encode($paints) . '}';
-        echo json_encode($paints);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function getPaint($id) {
-    $sql = "SELECT * FROM paints WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $paint = $stmt->fetchObject();
-        $db = null;
-        echo json_encode($paint);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function addPaint() {
-    $request = Slim::getInstance()->request();
-    $paint = json_decode($request->getBody());
-    $sql = "INSERT INTO paints (name, year, description, picture) VALUES (:name, :year, :description, :picture)";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("name", $paint->name);
-        $stmt->bindParam("year", $paint->year);
-        $stmt->bindParam("description", $paint->description);
-        $stmt->bindParam("picture", $paint->picture);
-        $stmt->execute();
-        $paint->id = $db->lastInsertId();
-        $db = null;
-        echo json_encode($paint);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function updatePaint($id) {
-    $request = Slim::getInstance()->request();
-    $body = $request->getBody();
-    $paint = json_decode($body);
-    $sql = "UPDATE paints SET name=:name, year=:year, description=:description, picture=:picture  WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("name", $paint->name);
-        $stmt->bindParam("year", $paint->year);
-        $stmt->bindParam("description", $paint->description);
-        $stmt->bindParam("picture", $paint->picture);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $db = null;
-        echo json_encode($paint);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function deletePaint($id) {
-	$sql = "SELECT * FROM paints WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $paint = $stmt->fetchObject();
-        $db = null;
-        //echo json_encode($paint);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-	
-    $sql = "DELETE FROM paints WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $db = null;
-		echo json_encode($paint);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function findByName($query) {
-    $sql = "SELECT * FROM paints WHERE UPPER(name) LIKE :query ORDER BY name";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $query = "%".$query."%";
-        $stmt->bindParam("query", $query);
-        $stmt->execute();
-        $paints = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        echo '{"paint": ' . json_encode($paints) . '}';
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
+  
 ?>
